af44b32c8f76139dc51260a0c1f4c07ef92eadc2
Niket1-1 <82999578+Niket1-1@users.noreply.github.com>
2021-12-25 18:50:42 +0530

    final

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app= express();
 const server =require("http").createServer(app);
 
 const io=require("socket.io")(server);
-const port=process.env.PORT || 5000
+const port=process.env.PORT || 5000;
 
 
 
